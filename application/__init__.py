3ea31fcd67873d4a183d00850a9b829d8d52687a
FernandoMarcon <fmarcon13@gmail.com>
2021-09-13 23:31:12 -0300

    try fix

--- application/__init__.py
+++ application/__init__.py
@@ -2,12 +2,10 @@ from flask import Flask
 from config import Config
 from flask_mongoengine import MongoEngine
 
-# api = Api()
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = MongoEngine()
 db.init_app(app)
-# Api.init_app(app)
 
 from application import routes
